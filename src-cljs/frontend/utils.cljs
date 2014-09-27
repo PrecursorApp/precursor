3648e10e2a61d3a317468348dc113f7f4371355f
Sean Grove <s@bushi.do>
2014-09-27 13:29:28 -0700

    utils/log-pr

--- src-cljs/frontend/utils.cljs
+++ src-cljs/frontend/utils.cljs
@@ -56,6 +56,12 @@
   (when logging-enabled?
     (.apply (.-error js/console) js/console (clj->js messages))))
 
+(defn log-pr [& args]
+  (apply print (map #(if (= (type %) (type ""))
+                     %
+                     (subs (pr-str %) 0 1000))
+                  args)))
+
 (defn uuid
   "returns a type 4 random UUID: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
   []
