40acf4c85caac89dfa7ae177190a9f4c76853d07
Daniel Woelfel <dwwoelfel@gmail.com>
2014-09-28 03:47:51 -0700

    add a debug function until we hook up sente to app-state

--- src-cljs/frontend/sente.cljs
+++ src-cljs/frontend/sente.cljs
@@ -1,6 +1,7 @@
 (ns frontend.sente
   (:require-macros [cljs.core.async.macros :as asyncm :refer (go go-loop)])
   (:require [cljs.core.async :as async :refer (<! >! put! chan)]
+            [frontend.utils :as utils :include-macros true]
             [taoensso.sente  :as sente :refer (cb-success?)]))
 
 (defn subscribe-to-document [sente-state document-id]
@@ -15,7 +16,17 @@
                      ((:send-fn sente-state) [:frontend/subscribe {:document-id document-id}])
                      (async/close! tap))))))
 
+(defn do-something [sente-state]
+  (let [tap (async/chan (async/sliding-buffer 1))
+         mult (async/mult (:ch-recv sente-state))]
+    (async/tap mult tap)
+    (go-loop []
+             (when-let [val (<! tap)]
+               (utils/inspect val)
+               (recur)))))
+
 (defn init [app-state]
   (let [{:keys [chsk ch-recv send-fn state] :as sente-state} (sente/make-channel-socket! "/chsk" {:type :auto})]
     (swap! app-state assoc :sente sente-state)
-    (subscribe-to-document sente-state 123456789)))
+    (subscribe-to-document sente-state 123456789)
+    (do-something sente-state)))
