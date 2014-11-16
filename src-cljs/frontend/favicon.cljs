64542ae918c88b4ec055fbc6d2fb975015a10c14
Daniel Woelfel <dwwoelfel@gmail.com>
2014-11-16 09:54:43 -0800

    bust favicon cache

--- src-cljs/frontend/favicon.cljs
+++ src-cljs/frontend/favicon.cljs
@@ -5,10 +5,12 @@
   (:require-macros [dommy.macros :refer [sel sel1]]))
 
 (def favicon-query "link[rel='icon']")
+;; increment version to break Chrome's aggressive caching
+(def version "1")
 
 (defn set-favicon! [path]
   (utils/swallow-errors
-   (attrs/set-attr! (sel1 favicon-query) :href path)))
+   (attrs/set-attr! (sel1 favicon-query) :href (str path "?v=" version))))
 
 (defn set-unread! []
   (set-favicon! "/favicon-notification.ico"))
