aef76cb63c043262cccc6f643736796960acdaf0
Daniel Woelfel <dwwoelfel@gmail.com>
2014-11-21 00:31:39 -0800

    fix logic in subject

--- src/pc/email.clj
+++ src/pc/email.clj
@@ -5,18 +5,19 @@
 (defn send-chat-invite [{:keys [cust to-email doc-id]}]
   (mailgun/send-message {:from "Precursor <draw@prcrsr.com>"
                          :to to-email
-                         :subject (str (:cust/first-name cust)
-                                       (when (:cust/first-name cust)
-                                         (str " " (:cust/last-name cust)))
-                                       " "
-                                       (cond (and (not (:cust/last-name cust))
-                                                  (:cust/first-name cust))
-                                             (str "(" (:cust/email cust) ") ")
+                         :subject (str/trim (str (:cust/first-name cust)
+                                                 (when (and (:cust/first-name cust)
+                                                            (:cust/last-name cust))
+                                                   (str " " (:cust/last-name cust)))
+                                                 " "
+                                                 (cond (and (not (:cust/last-name cust))
+                                                            (:cust/first-name cust))
+                                                       (str "(" (:cust/email cust) ") ")
 
-                                             (and (not (:cust/last-name cust))
-                                                  (not (:cust/first-name cust)))
-                                             (str (:cust/email cust) " ")
-                                             :else nil)
-                                       "invited you to a document on Precursor")
+                                                       (not (:cust/first-name cust))
+                                                       (str (:cust/email cust) " ")
+
+                                                       :else nil)
+                                                 "invited you to a document on Precursor"))
                          :text (str "Hey there,\nCome draw with me on Precursor: https://prcrsr.com/document" doc-id)
                          :html (format "<html><body><div>Hey there,</div><br /><div>Come draw with me on Precursor: <a href=\"https://prcrsr.com/document/%s\">https://prcrsr.com/document/%s</a></div></body></html>" doc-id doc-id)}))
