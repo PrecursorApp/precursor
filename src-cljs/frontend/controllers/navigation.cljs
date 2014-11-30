eeb9cbb870c8ee2cb949336b6b320d6cac8c57e9
Daniel Woelfel <dwwoelfel@gmail.com>
2014-11-30 14:16:06 -0800

    routing!

--- src-cljs/frontend/controllers/navigation.cljs
+++ src-cljs/frontend/controllers/navigation.cljs
@@ -2,17 +2,17 @@
   (:require [cljs.core.async :as async :refer [>! <! alts! chan sliding-buffer close!]]
             [clojure.string :as str]
             [frontend.async :refer [put!]]
+            [frontend.db :as db]
             [frontend.state :as state]
             [frontend.stefon :as stefon]
             [frontend.utils.ajax :as ajax]
             [frontend.utils.state :as state-utils]
             [frontend.utils.vcs-url :as vcs-url]
-            [frontend.utils :as utils :refer [mlog merror]]
+            [frontend.utils :as utils :include-macros true]
             [goog.dom]
             [goog.string :as gstring]
             [goog.style])
-  (:require-macros [frontend.utils :refer [inspect]]
-                   [dommy.macros :refer [sel sel1]]
+  (:require-macros [dommy.macros :refer [sel sel1]]
                    [cljs.core.async.macros :as am :refer [go go-loop alt!]]))
 
 ;; TODO we could really use some middleware here, so that we don't forget to
@@ -24,25 +24,8 @@
 (defn set-page-title! [& [title]]
   (set! (.-title js/document) (utils/strip-html
                                (if title
-                                 (str title  " - CircleCI")
-                                 "CircleCI"))))
-
-(defn scroll-to-fragment!
-  "Scrolls to the element with id of fragment, if one exists"
-  [fragment]
-  (when-let [node (goog.dom.getElement fragment)]
-    (let [body (sel1 "body")
-          node-top (goog.style/getPageOffsetTop node)
-          body-top (goog.style/getPageOffsetTop body)]
-      (set! (.-scrollTop body) (- node-top body-top)))))
-
-(defn scroll!
-  "Scrolls to fragment if the url had one, or scrolls to the top of the page"
-  [args]
-  (if (:_fragment args)
-    ;; give the page time to render
-    (utils/rAF #(scroll-to-fragment! (:_fragment args)))
-    (utils/rAF #(set! (.-scrollTop (sel1 "body")) 0))))
+                                 (str title  " - Precursor")
+                                 "Precursor - Simple collaborative prototyping"))))
 
 ;; --- Navigation Multimethod Declarations ---
 
@@ -51,7 +34,6 @@
 
 (defmulti post-navigated-to!
   (fn [history-imp navigation-point args previous-state current-state]
-    (put! (get-in current-state [:comms :ws]) [:unsubscribe-stale-channels])
     navigation-point))
 
 ;; --- Navigation Multimethod Implementations ---
@@ -63,16 +45,12 @@
 
 (defmethod navigated-to :default
   [history-imp navigation-point args state]
+  (utils/mlog "No navigated-to for" navigation-point)
   (navigated-default navigation-point args state))
-
-(defn post-default [navigation-point args]
-  (set-page-title! (or (:_title args)
-                       (str/capitalize (name navigation-point))))
-  (scroll! args))
 
 (defmethod post-navigated-to! :default
   [history-imp navigation-point args previous-state current-state]
-  (post-default navigation-point args))
+  (utils/mlog "No post-navigated-to! for" navigation-point))
 
 (defmethod navigated-to :navigate!
   [history-imp navigation-point args state]
@@ -86,3 +64,21 @@
     (if replace-token? ;; Don't break the back button if we want to redirect someone
       (.replaceToken history-imp path)
       (.setToken history-imp path))))
+
+(defmethod navigated-to :document
+  [history-imp navigation-point args state]
+  (-> (navigated-default navigation-point args state)
+      (assoc :document/id (:document/id args)
+             :undo-state (atom {:transactions []
+                                :last-undo nil}))
+      (update-in [:db] db/reset-db!)))
+
+(defmethod post-navigated-to! :document
+  [history-imp navigation-point _ previous-state current-state]
+  ;; TODO: get rid of old db
+  (db/setup-listener! (:db current-state)
+                      (fn [message data & [transient?]]
+                        (put! (get-in current-state [:comms :controls]) [message data transient?]))
+                      (:document/id current-state)
+                      (:undo-state current-state)
+                      (:sente current-state)))
