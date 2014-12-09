7050c76267a21becf8440839d1005532694303b1
Daniel Woelfel <dwwoelfel@gmail.com>
2014-12-08 22:37:32 -0800

    fix parens

--- src-cljs/frontend/controllers/controls.cljs
+++ src-cljs/frontend/controllers/controls.cljs
@@ -774,8 +774,7 @@
         (assoc-in [:drawing :in-progress?] false)
         (assoc-in [:camera :offset-x] (if aside-open?
                                         (get-in state state/aside-width-path)
-                                        0))
-        )))
+                                        0)))))
 
 (defmethod post-control-event! :aside-menu-toggled
   [browser-state message _ previous-state current-state]
