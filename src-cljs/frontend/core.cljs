8960afee77fa064886240073fc6e3b934c788f92
Daniel Woelfel <dwwoelfel@gmail.com>
2015-01-14 14:29:10 -0800

    case-insensitive check for input or textarea

--- src-cljs/frontend/core.cljs
+++ src-cljs/frontend/core.cljs
@@ -69,7 +69,7 @@
                        (js/String.fromCharCode (.-which event)))
         tokens     [shift? meta? ctrl? alt? char]
         key-string (string/join "+" (filter identity tokens))]
-    (when-not (contains? #{"INPUT" "TEXTAREA"} (.. event -target -tagName))
+    (when-not (contains? #{"input" "textarea"} (string/lower-case (.. event -target -tagName)))
       (when (get suppressed-key-combos key-string)
         (.preventDefault event))
       (when-not (.-repeat event)
