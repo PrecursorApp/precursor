c901ca53603d559940a108b6d4bd7750fb9aa250
Daniel Woelfel <dwwoelfel@gmail.com>
2015-01-19 19:55:00 -0800

    make gifs start when you hover

--- src/pc/views/blog/private_docs_early_access.clj
+++ src/pc/views/blog/private_docs_early_access.clj
@@ -2,6 +2,18 @@
   (:require [ring.middleware.anti-forgery :refer (wrap-anti-forgery)]
             [pc.views.blog.common :as common]))
 
+
+(defn demo [placeholder gif & {:keys [caption]}]
+  [:figure.play-gif {:alt "demo"
+                     :onmouseover (format "this.getElementsByTagName('img')[0].src = '%s'" gif)
+                     :onmouseout (format "this.getElementsByTagName('img')[0].src = '%s'" placeholder)}
+   [:a (when caption
+         {:data-caption-black caption})
+    [:img {:src placeholder}]]])
+
+(defn demo-with-blank-canvas [gif caption]
+  (demo "/blog/private-docs-early-access/canvas.png" gif :caption caption))
+
 (defn private-docs-early-access []
   {:title "Private docs early access."
    :blurb "How to access and use our new privacy settings"
@@ -19,39 +31,28 @@
      [:p "Open the side menu with the button in the upper-left corner and then select \"Sharing\".
          At the bottom of the sharing menu, hover over the Private/Public toggle to expand it, then select \"Private\".
          The owner of the doc can toggle between public and private at anytime from this menu."]]
-    [:figure.play-gif {:alt "demo"}
-     [:a {:data-caption-black "Make a doc private by toggling the privacy setting at the bottom of the sharing menu."}
-      [:img {:src "/blog/private-docs-early-access/make-private.gif"}]
-      [:img {:src "/blog/private-docs-early-access/canvas.png"}]]]
+    (demo-with-blank-canvas "/blog/private-docs-early-access/make-private.gif" "Make a doc private by toggling the privacy setting at the bottom of the sharing menu.")
 
     [:article
      [:h3 "How do I grant access to my private doc?"]
      [:p "After the doc is made private, the creator of the doc will be the only user allowed to view its contents.
          To grant access to other users, simply enter their email in the input field near the top of the sharing menu and submit it with enter.
          That user will then be displayed in a list below the input field."]]
-    [:figure.play-gif {:alt "demo"}
-     [:a {:data-caption-black "Grant someone access with their email address."}
-      [:img {:src "/blog/private-docs-early-access/grant-access.gif"}]
-      [:img {:src "/blog/private-docs-early-access/canvas.png"}]]]
+    (demo-with-blank-canvas "/blog/private-docs-early-access/grant-access.gif" "Grant someone access with their email address.")
 
     [:article
      [:h3 "How do I approve access to my private doc?"]
      [:p "When someone requests access to your private doc, they will show up in a list inside the sharing menu.
          From this menu you can approve or deny requests by hovering over them and clicking either the check or the x icon.
          If you deny someone by mistake, you will still have the option to approve them."]]
-    [:figure.play-gif {:alt "demo"}
-     [:a {:data-caption-black "Approve or deny access requests from the sharing menu."}
-      [:img {:src "/blog/private-docs-early-access/approve-access.gif"}]
-      [:img {:src "/blog/private-docs-early-access/canvas.png"}]]]
+    (demo-with-blank-canvas "/blog/private-docs-early-access/approve-access.gif" "Approve or deny access requests from the sharing menu.")
 
     [:article
      [:h3 "How do I request access to a private doc?"]
      [:p "When other users arrive at the url for a private doc they will find an empty canvas and a prompt explaining that the doc is private.
          That prompt has a button which allows them to request access from the owner."]]
-    [:figure.play-gif {:alt "demo"}
-     [:a {:data-caption-black "Request access by going to the url and following the menu prompt."}
-      [:img {:src "/blog/private-docs-early-access/request-access.gif"}]
-      [:img {:src "/blog/private-docs-early-access/canvas.png"}]]]
+
+    (demo-with-blank-canvas "/blog/private-docs-early-access/request-access.gif" "Request access by going to the url and following the menu prompt.")
 
     [:article
      [:h3 "What kind of feedback should I share?"]
