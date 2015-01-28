de89d4e28bf4983c586a8896b2fe1c88e993fc98
Daniel Woelfel <dwwoelfel@gmail.com>
2015-01-27 19:42:21 -0800

    make 60 the baseline for entity ids

--- src-cljs/frontend/core.cljs
+++ src-cljs/frontend/core.cljs
@@ -291,7 +291,7 @@
 
 (defn setup-entity-id-fetcher [state]
   (let [api-ch (-> state deref :comms :api)]
-    (fetch-entity-ids api-ch 40)
+    (fetch-entity-ids api-ch 60)
     (add-watch state :entity-id-fetcher (fn [key ref old new]
                                           (when (> 20 (-> new :entity-ids count))
                                             (println "fetching more entity ids")
