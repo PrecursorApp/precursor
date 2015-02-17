63a43f7cea3b0346002fc25c9b43dc958e39ed32
Daniel Woelfel <dwwoelfel@gmail.com>
2015-02-17 00:44:29 -0800

    generator was a bad idea, client-id fn should accept a state that is stored elsewhere

--- src-cljs/frontend/db.cljs
+++ src-cljs/frontend/db.cljs
@@ -34,3 +34,31 @@
          (doseq [datom-group (partition-all 500 datoms)]
            (sente/send-msg sente-state [:frontend/transaction {:datoms datom-group
                                                                :document/id document-id}])))))))
+
+(defn generate-entity-id
+  "Assumes that each entity has a different remainder and that all clients have
+   the same multiple.
+   Takes a db to guard against duplicate ids, but is not meant to be a protection
+   against races. Only guards against a previous client with the same remainder skipping
+   ids (probably due to an error).
+   Returns a map of :entity-id and new :frontend-id-state"
+  [db {:keys [multiple remainder next-id] :as frontend-id-state}]
+  {:entity-id next-id
+   :frontend-id-state (assoc frontend-id-state :next-id (loop [res (+ next-id multiple)]
+                                                          (if (first (d/datoms db :eavt res))
+                                                            (recur (+ res multiple))
+                                                            res)))})
+
+(defn generate-entity-ids
+  "Same as generate-entity-id, but returns a list of :entity-ids"
+  [db id-count frontend-id-state]
+  (loop [i 0
+         entity-ids []
+         id-state frontend-id-state]
+    (if (>= i id-count)
+      {:entity-ids entity-ids
+       :frontend-id-state id-state}
+      (let [res (generate-entity-id db id-state)]
+        (recur (inc i)
+               (conj entity-ids (:entity-id res))
+               (:frontend-id-state res))))))
