2282c4126a174d8caacda9be4e7a5ce751f175ef
Daniel Woelfel <dwwoelfel@gmail.com>
2015-02-17 03:20:25 -0800

    replace all of the hardcoded prcrsr.com

--- src/pc/email.clj
+++ src/pc/email.clj
@@ -6,13 +6,21 @@
             [datomic.api :as d]
             [hiccup.core :as hiccup]
             [pc.datomic :as pcd]
+            [pc.http.urls :as urls]
             [pc.mailgun :as mailgun]
             [pc.models.access-grant :as access-grant-model]
             [pc.models.permission :as permission-model]
             [pc.models.cust :as cust-model]
             [pc.models.doc :as doc-model]
+            [pc.profile :as profile]
             [pc.utils]
             [slingshot.slingshot :refer (throw+ try+)]))
+
+(defn email-address
+  ([local-part]
+   (format "%s@%s" local-part (profile/prod-domain)))
+  ([fancy-name local-part]
+   (format "%s <%s@%s>" fancy-name local-part (profile/prod-domain))))
 
 (defn emails-to-send [db eid]
   (set (map first
@@ -47,19 +55,19 @@
     [:body
      [:p
       "I'm prototyping something on Precursor, come join me at "
-      [:a {:href (str "https://prcrsr.com/document/" doc-id)}
-       (str "https://prcrsr.com/document/" doc-id)]
+      [:a {:href (urls/doc doc-id)}
+       (urls/doc doc-id)]
       "."]
      [:p "This is what I have so far:"]
      [:p
-      [:a {:href (str "https://prcrsr.com/document/" doc-id)
+      [:a {:href (urls/doc doc-id)
            :style "display: inline-block"}
        [:img {:width 325
               :style "border: 1px solid #888888;"
               :alt "Images disabled? Just come and take a look."
-              :src (str "https://prcrsr.com/document/" doc-id ".png?rand=" (rand))}]]]
+              :src (urls/doc-png doc-id :query {:rand (rand)})}]]]
      [:p {:style "font-size: 12px"}
-      "Tell us if this message was sent in error info@prcrsr.com."
+      (format "Tell us if this message was sent in error %s." (email-address "info"))
       ;; Add some hidden text so that Google doesn't try to trim these.
       [:span {:style "display: none; max-height: 0px; font-size: 0px; overflow: hidden;"}
        " Sent at "
@@ -92,11 +100,11 @@
                    (when-not (str/blank? full-name) ")")))))
 
 (defn send-chat-invite [{:keys [cust to-email doc-id]}]
-  (mailgun/send-message {:from "Precursor <joinme@prcrsr.com>"
+  (mailgun/send-message {:from (email-address "Precursor" "joinme")
                          :to to-email
                          :subject (str (format-inviter cust)
                                        " invited you to a document on Precursor")
-                         :text (str "Hey there,\nCome draw with me on Precursor: https://prcrsr.com/document" doc-id)
+                         :text (str "Hey there,\nCome draw with me on Precursor: " (urls/doc doc-id))
                          :html (chat-invite-html doc-id)
                          :o:tracking "yes"
                          :o:tracking-opens "yes"
@@ -104,15 +112,15 @@
                          :o:campaign "chat_invites"}))
 
 (defn access-grant-html [doc-id access-grant image-permission]
-  (let [doc-link (str "https://prcrsr.com/document/" doc-id "?access-grant-token=" (:access-grant/token access-grant))
-        image-link (str "https://prcrsr.com/document/" doc-id ".png?rand=" (rand) "&auth-token=" (:permission/token image-permission))]
+  (let [doc-link (urls/doc doc-id :query {:access-grant-token (:access-grant/token access-grant)})
+        image-link (urls/doc-png doc-id :query {:rand (rand) :auth-token (:permission/token image-permission)})]
     (hiccup/html
      [:html
       [:body
        [:p
         "I'm prototyping something on Precursor, come join me at "
         [:a {:href doc-link}
-         (str "https://prcrsr.com/document/" doc-id)]
+         (urls/doc doc-id)]
         "."]
        [:p "This is what I have so far:"]
        [:p
@@ -123,7 +131,7 @@
                 :alt "Images disabled? Just come and take a look."
                 :src image-link}]]]
        [:p {:style "font-size: 12px"}
-        "Tell us if this message was sent in error info@prcrsr.com."
+        (format "Tell us if this message was sent in error %s." (email-address "info"))
         ;; Add some hidden text so that Google doesn't try to trim these.
         [:span {:style "display: none; max-height: 0px; font-size: 0px; overflow: hidden;"}
          " Sent at "
@@ -136,11 +144,11 @@
         granter (access-grant-model/get-granter db access-grant)
         token (:access-grant/token access-grant)
         image-permission (permission-model/create-document-image-permission! {:db/id doc-id})]
-    (mailgun/send-message {:from "Precursor <joinme@prcrsr.com>"
+    (mailgun/send-message {:from (email-address "Precursor" "joinme")
                            :to (:access-grant/email access-grant)
                            :subject (str (format-inviter granter)
                                          " invited you to a document on Precursor")
-                           :text (str "Hey there,\nCome draw with me on Precursor: https://prcrsr.com/document" doc-id
+                           :text (str "Hey there,\nCome draw with me on Precursor: " (urls/doc doc-id)
                                       "?access-grant-token=" token)
                            :html (access-grant-html doc-id access-grant image-permission)
                            :o:tracking "yes"
@@ -149,15 +157,15 @@
                            :o:campaign "access_grant_invites"})))
 
 (defn permission-grant-html [doc-id image-permission]
-  (let [doc-link (str "https://prcrsr.com/document/" doc-id)
-        image-link (str "https://prcrsr.com/document/" doc-id ".png?rand=" (rand) "&auth-token=" (:permission/token image-permission))]
+  (let [doc-link (urls/doc doc-id)
+        image-link (urls/doc-png doc-id :query {:rand (rand) :auth-token (:permission/token image-permission)})]
     (hiccup/html
      [:html
       [:body
        [:p
         "I'm prototyping something on Precursor, come join me at "
         [:a {:href doc-link}
-         (str "https://prcrsr.com/document/" doc-id)]
+         doc-link]
         "."]
        [:p "This is what I have so far:"]
        [:p
@@ -168,7 +176,7 @@
                 :alt "Images disabled? Just come and take a look."
                 :src image-link}]]]
        [:p {:style "font-size: 12px"}
-        "Tell us if this message was sent in error info@prcrsr.com."
+        (str "Tell us if this message was sent in error %s." (email-address "info"))
         ;; Add some hidden text so that Google doesn't try to trim these.
         [:span {:style "display: none; max-height: 0px; font-size: 0px; overflow: hidden;"}
          " Sent at "
@@ -181,11 +189,11 @@
         granter (permission-model/get-granter db permission)
         grantee (d/entity db (:permission/cust permission))
         image-permission (permission-model/create-document-image-permission! {:db/id doc-id})]
-    (mailgun/send-message {:from "Precursor <joinme@prcrsr.com>"
+    (mailgun/send-message {:from (email-address "Precursor" "joinme")
                            :to (:cust/email grantee)
                            :subject (str (format-inviter granter)
                                          " gave you access to a document on Precursor")
-                           :text (str "Hey there,\nCome draw with me on Precursor: https://prcrsr.com/document" doc-id)
+                           :text (str "Hey there,\nCome draw with me on Precursor: " (urls/doc doc-id))
                            :html (permission-grant-html doc-id image-permission)
                            :o:tracking "yes"
                            :o:tracking-opens "yes"
@@ -194,17 +202,17 @@
 
 
 (defn access-request-html [doc-id requester]
-  (let [doc-link (str "https://prcrsr.com/document/" doc-id)]
+  (let [doc-link (urls/doc doc-id)]
     (hiccup/html
      [:html
       [:body
        [:p (str (format-requester requester) " wants access to one of your documents on Precursor.")]
        [:p "Go to the "
-        [:a {:href (str "https://prcrsr.com/document/" doc-id "?overlay=sharing")}
+        [:a {:href (urls/doc doc-id :query {:overlay "sharing"})}
          "manage permissions page"]
         " to grant or deny access."]
        [:p {:style "font-size: 12px"}
-        "Tell us if this message was sent in error info@prcrsr.com."
+        (format "Tell us if this message was sent in error %s." (email-address "info"))
         ;; Add some hidden text so that Google doesn't try to trim these.
         [:span {:style "display: none; max-height: 0px; font-size: 0px; overflow: hidden;"}
          " Sent at "
@@ -217,11 +225,11 @@
         doc (doc-model/find-by-id db (:access-request/document access-request))
         doc-id (:db/id doc)
         doc-owner (cust-model/find-by-uuid db (:document/creator doc))]
-    (mailgun/send-message {:from "Precursor <joinme@prcrsr.com>"
+    (mailgun/send-message {:from (email-address "Precursor" "joinme")
                            :to (:cust/email doc-owner)
                            :subject (str (format-requester requester)
                                          " wants access to your document on Precursor")
-                           :text (str "Hey there,\nSomeone wants access to your document on Precursor: https://prcrsr.com/document" doc-id
+                           :text (str "Hey there,\nSomeone wants access to your document on Precursor: " (urls/doc doc-id)
                                       "\nYou can grant or deny them access from the document's settings page.")
                            :html (access-request-html doc-id requester)
                            :o:tracking "yes"
