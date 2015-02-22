429041d5c60ae1abcec9832516ceb3e1f19f43ac
Daniel Woelfel <dwwoelfel@gmail.com>
2015-02-21 16:00:19 -0800

    new transactor fn to retract an entity without retracting the frontend/id

--- src/pc/datomic/web_peer.clj
+++ src/pc/datomic/web_peer.clj
@@ -44,3 +44,7 @@
 
 (defn server-frontend-id [entity-id namespace-part]
   [::assign-frontend-id entity-id namespace-part multiple reserved-remainder])
+
+(defn retract-entity [entity-id]
+  [::retract-entity entity-id])
+
