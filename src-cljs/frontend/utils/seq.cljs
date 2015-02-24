c5de005187ddd7424f09f6c7552bc481f526f7b2
Daniel Woelfel <dwwoelfel@gmail.com>
2015-02-23 19:58:21 -0800

    use a empty version of the map passed to select-in
    we want to preserve om cursors

--- src-cljs/frontend/utils/seq.cljs
+++ src-cljs/frontend/utils/seq.cljs
@@ -11,7 +11,7 @@
   "Returns a map containing only those entries in map whose keypath is in keypaths
    (select-in {:a {:b 1 :c 2}} [[:a :b]]) => {:a {:b 1}} "
   [map keypathseq]
-  (loop [ret {} keypaths (seq keypathseq)]
+  (loop [ret (empty map) keypaths (seq keypathseq)]
     (if keypaths
       (let [entry (get-in map (first keypaths) sentinel)]
         (recur
