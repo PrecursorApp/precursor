6d9f1b7dba40c151fe719797a8fb5383ea899877
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-02 11:29:49 -0800

    updates based on feedback

--- src/pc/views/blog/optimizing_om_apps.clj
+++ src/pc/views/blog/optimizing_om_apps.clj
@@ -1,4 +1,6 @@
-(ns pc.views.blog.optimizing-om-apps)
+(ns pc.views.blog.optimizing-om-apps
+  (:require [pc.views.blog.common :as common]
+            [pc.views.common :refer (cdn-path)]))
 
 (def will-mount-code-block
 "(defn wrap-will-mount
@@ -42,20 +44,28 @@
    :body
    (list
     [:article
-     [:p "Performance is very important for a prototyping tool, particularly one built for the web. This forces me to spend a lot of time thinking about browser performance for "
+     [:p "Performance is very important for a prototyping tool, particularly one built on the web. This forces me to spend a lot of time thinking about browser performance for "
       [:a {:href "https://precursorapp.com"} "Precursor"]
-      ". Our goal at Precursor is to make collaborating on ideas effortless. If things don't behave the way our users expect, we run the risk of pushing them backwards—retreating towards alternatives that are more familiar, like pen and paper or whiteboards."]
-     [:p "We built our front end with "
+      ". Our goal at Precursor is to make collaborating on ideas effortless. If things don't behave the way users expect, there's a risk of pushing them backwards—retreating towards alternatives that are more familiar, like pen and paper or whiteboards."]
+     [:p "Our front end is build with "
       [:a {:href "https://github.com/omcljs/om"} "Om"]
       " and "
       [:a {:href "https://github.com/facebook/react"} "React"]
       "; both come with excellent performance right out of the box. React is well-known for minimizing the amount of slow DOM updates with their virtual DOM diffing. Om improves on that by "
-      [:a {:href "https://swannodette.github.io/2013/12/17/the-future-of-javascript-mvcs/"} "leveraging fast comparisons of Clojure’s persistent data structures"]
+      [:a {:href "https://swannodette.github.io/2013/12/17/the-future-of-javascript-mvcs/"} "leveraging fast comparisons of Closure’s persistent data structures"]
       "."]
 
-     [:p "Unfortunately, the defaults weren't good enough for us. I noticed that Precursor felt sluggish when too many people were drawing in the same document at the same time, and when the app was used on mobile browsers. We traced most of these problems to components rendering unnecessarily. For example, the chat component would re-render every time a new drawing was created."]
+     [:p "As good as they are, the defaults weren't good enough for us. I noticed that Precursor felt sluggish when too many people were drawing in the same document together, and when drawing on mobile browsers. I traced most of these problems to components rendering unnecessarily. For example, the chat component would re-render every time a new drawing was created."]
 
-     [:p "I built some instrumentation tools to help identify components that get passed too much data or are slow to render. These tools have been invaluable while optimizing Precursor, and we've decided to open-source them so anyone can include them in their own Om projects."]
+     [:p "To get a sense for performance bottlenecks, you generally want to see:"]
+
+     [:p
+      [:ul
+       [:li "How many times a component is mounted/unmounted"]
+       [:li "How many times a component is rendered (to determine when it’s being rendered unnecessarily)"]
+       [:li "How long each component takes to render"]]]
+
+     [:p "I built some instrumentation tools to display this information in an actionable format while interacting with the app. These tools have been invaluable while optimizing Precursor, and we've decided to open-source them so anyone can include them in their own Om projects."]
 
      [:p "I'm calling the library Om-i (pronounced “Oh, my!”), which is short for Om-instrumentation. You can find "
       [:a {:href "https://github.com/PrecursorApp/Om-i"}
@@ -65,6 +75,9 @@
        "Precursor"]
       " by hitting Ctrl+Shift+Alt+J."]]
 
+    ;; n.b. this image is only stored in the CDN, it's not available locally
+    (common/demo (cdn-path "/blog/optimizing-om-apps/instrumentation-frame.png") "https://dtwdl3ecuoduc.cloudfront.net/om-i/instrumentation.gif")
+
     [:article
      [:h3 "How it works"]
      [:p "Om lets you specify custom handlers for React's component lifecycle methods. First, we track mount times by wrapping Om's default componentWillMount and componentDidMount methods. Then in componentWillMount, we associate the start time with the component's React id, and calculate the mount time when componentDidMount fires. We also do the same with the update lifecycle methods to get render times."]
@@ -73,11 +86,12 @@
       [:span did-mount-code-block]]
      [:p "Every mount and update gets stored to keep track of average and maximum render times."]]
 
-    ;; n.b. that this image is only stored in the CDN, it's not available locally
-    [:figure [:img {:src "https://dtwdl3ecuoduc.cloudfront.net/om-i/om-i-demo.gif"}]]
-
     [:article
      [:p
       "This library was based on previous work, "
       [:a {:href "https://dwwoelfel.github.io/instrumenting-om-components.html"} "Instrumenting Om Components"]
-      "."]])})
+      "."]
+     [:p "Thanks to "
+      [:a {:href "https://twitter.com/sgrove"}
+       "Sean Grove"]
+      " for reading drafts of this."]])})
