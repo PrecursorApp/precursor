ae9b2d9102c7cb7cc3d2e180c4fa1015abb1253f
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-05 03:48:34 -0800

    stop sending chat/cust-name through the api

--- src/pc/models/chat.clj
+++ src/pc/models/chat.clj
@@ -16,20 +16,12 @@
                db cust-uuid)))
 
 (defn find-by-document [db document]
-  (let [memo-find-name (memoize find-chat-name)]
-    (map
-     (fn [[chat-id]]
-       (let [e (pcd/touch+ (d/entity db chat-id))]
-         ;; TODO: teach the frontend how to lookup cust/name
-         (let [name (when-let [uuid (:cust/uuid e)]
-                      (memo-find-name db uuid))]
-           (if name
-             (assoc e :chat/cust-name name)
-             e))))
-     (d/q '{:find [?t] :in [$ ?document-id]
-            :where [[?t :document/id ?document-id]
-                    [?t :chat/body]]}
-          db (:db/id document)))))
+  (map (partial d/entity db)
+       (d/q '{:find [[?t ...]]
+              :in [$ ?document-id]
+              :where [[?t :document/id ?document-id]
+                      [?t :chat/body]]}
+            db (:db/id document))))
 
 ;; TODO: move cust-name lookup into here
 (defn read-api [chat]
@@ -37,11 +29,8 @@
     (select-keys [:document/id
                   :server/timestamp
                   :client/timestamp
-                  ;; TODO: teach frontend to lookup cust/name
                   :session/uuid
-                  :cust/cust-name
                   :chat/body
                   :chat/color
-                  :chat/cust-name
                   :cust/uuid])
     (assoc :db/id (web-peer/client-id chat))))
