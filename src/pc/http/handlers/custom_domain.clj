e96828420edc31e97c1859985a753353eb3cb37c
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-07 16:00:14 -0800

    custom domain handler just adds fields to the request map

--- src/pc/http/handlers/custom_domain.clj
+++ src/pc/http/handlers/custom_domain.clj
@@ -1,29 +1,24 @@
 (ns pc.http.handlers.custom-domain
   (:require [cemerick.url :as url]
-            [pc.http.urls :as urls]
-            [pc.profile :as profile]
-            [ring.middleware.anti-forgery]))
+            [pc.datomic :as pcd]
+            [pc.models.team :as team-model]
+            [pc.profile :as profile]))
 
+;; XXX: redirect for things on the protected list
 (def blacklist #{"www" "admin"})
 
 (defn parse-subdomain [req]
   (last (re-find (re-pattern (str "(.+)\\." (profile/hostname)))
                  (:server-name req))))
 
+(defn handle-custom-domains [handler req]
+  (let [subdomain (parse-subdomain req)]
+    (if-not subdomain
+      (handler req)
+      (handler (assoc req
+                      :subdomain subdomain
+                      :team (team-model/find-by-subdomain (pcd/default-db) subdomain))))))
+
 (defn wrap-custom-domains [handler]
   (fn [req]
-    (if-let [subdomain (parse-subdomain req)]
-      (cond
-        (get-in req [:auth :cust])
-        {:status 200
-         :body (str "Hi " (get-in req [:auth :cust :cust/email]) "!")}
-
-        (= "/auth/google" (:uri req))
-        (handler req)
-
-        :else
-        {:status 200
-         :body (format "<body><a href='%s'>Log in with Google</a></body>"
-                       (urls/make-url "/login" :query (url/map->query {:redirect-subdomain subdomain
-                                                                       :redirect-csrf-token ring.middleware.anti-forgery/*anti-forgery-token*})))})
-      (handler req))))
+    (handle-custom-domains handler req)))
