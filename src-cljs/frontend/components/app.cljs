43eab89d983007397a158bb67f32b7140280a168
danny <danny.newidea@gmail.com>
2015-03-11 23:08:27 -0700

    first pass at styling right side menu

--- src-cljs/frontend/components/app.cljs
+++ src-cljs/frontend/components/app.cljs
@@ -41,7 +41,7 @@
 
         (if-let [nav-point (:navigation-point app)]
           (html
-           [:div#app.app {:class (str (when (overlay-visible? app) " state-menu ")
+           [:div#app.app.state-menu-right {:class (str (when (overlay-visible? app) " state-menu ")
                                       (when (:show-landing? app) " state-outer "))}
             (when (:show-landing? app)
               (om/build outer/outer (select-in app [[:show-landing?]
