aa385c643003273917b9cb66a88889c6462c2594
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-13 23:12:22 -0700

    fix new api with subdomains

--- src/pc/http/routes/api.clj
+++ src/pc/http/routes/api.clj
@@ -1,16 +1,38 @@
 (ns pc.http.routes.api
-  (:require [clojure.tools.reader.edn :as edn]
+  (:require [cemerick.url :as url]
+            [clojure.tools.reader.edn :as edn]
             [defpage.core :as defpage :refer (defpage)]
+            [pc.auth :as auth]
+            [pc.datomic :as pcd]
             [pc.early-access]
+            [pc.http.handlers.custom-domain :as custom-domain]
             [pc.models.doc :as doc-model]
-            [pc.models.chat-bot :as chat-bot-model]))
+            [pc.models.chat-bot :as chat-bot-model]
+            [pc.profile :as profile]))
 
 (defpage new [:post "/api/v1/document/new"] [req]
-  (let [cust-uuid (get-in req [:auth :cust :cust/uuid])
-        doc (doc-model/create-public-doc!
-             (merge {:document/chat-bot (rand-nth chat-bot-model/chat-bots)}
-                    (when cust-uuid {:document/creator cust-uuid})))]
-    {:status 200 :body (pr-str {:document {:db/id (:db/id doc)}})}))
+  (if (:subdomain req)
+    (if (and (:team req)
+             (auth/logged-in? req)
+             (auth/has-team-permission? (pcd/default-db) (:team req) (:auth req) :admin))
+      (let [doc (doc-model/create-team-doc!
+                 (:team req)
+                 (merge {:document/chat-bot (rand-nth chat-bot-model/chat-bots)}
+                        (when-let [cust-uuid (get-in req [:cust :cust/uuid])]
+                          {:document/creator cust-uuid})))]
+        {:status 200 :body (pr-str {:document {:db/id (:db/id doc)}})})
+      {:status 400 :body (pr-str {:error :unauthorized-to-team
+                                  :redirect-url (str (url/map->URL {:host (profile/hostname)
+                                                                    :protocol (name (:scheme req))
+                                                                    :port (:server-port req)
+                                                                    :path "/new"
+                                                                    :query (:query-string req)}))
+                                  :msg "You're unauthorized to make documents in this subdomain. Please request access."})})
+    (let [cust-uuid (get-in req [:auth :cust :cust/uuid])
+          doc (doc-model/create-public-doc!
+               (merge {:document/chat-bot (rand-nth chat-bot-model/chat-bots)}
+                      (when cust-uuid {:document/creator cust-uuid})))]
+      {:status 200 :body (pr-str {:document {:db/id (:db/id doc)}})})))
 
 (defpage early-access [:post "/api/v1/early-access"] [req]
   (if-let [cust (get-in req [:auth :cust])]
