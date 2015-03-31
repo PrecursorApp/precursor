0ac5577ebc490ec199382430127965f3a3f27027
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-31 12:46:20 -0700

    move to id-wear-that and finsih post

--- src/pc/views/blog/id_wear_that.clj
+++ src/pc/views/blog/id_wear_that.clj
@@ -1,4 +1,4 @@
-(ns pc.views.blog.prototyping-tshirt-lobanovskiy
+(ns pc.views.blog.id-wear-that
   (:require [ring.middleware.anti-forgery :refer (wrap-anti-forgery)]
             [pc.http.urls :as urls]
             [pc.views.email :as email]
@@ -6,18 +6,20 @@
             [pc.views.common :refer (cdn-path external-cdn-path)]
             [pc.views.blog.common :as common]))
 
-(defn prototyping-tshirt-lobanovskiy []
+(defn id-wear-that []
   {:title "I'd wear that."
-   :blurb ""
+   :blurb "Show us the interesting thing you made on Precursor and you could get a T-shirt with your design..."
    :author "Danny"
-   :image (external-cdn-path "/blog/ideas-are-made-with-precursor/lobanovskiy.gif")
+   :image (external-cdn-path "/blog/ideas-are-made-with-precursor/eddie-shirt.png")
    :body
    (list
-
     [:article
-     [:p "Got something interesting, made with " [:a {:href "/new" :title "Make something."} "Precursor"] "? "]
-     [:p "Tweet " [:a {:href "https://twitter.com/PrecursorApp"} "@PrecursorApp"] " and include #madewithprecursor. "
-         " I'll send you a one-of-a-kind shirt for free, if we feature it next time! "]]
+     [:p "Got something interesting made with "
+      [:a {:href "/new" :title "Make something."} "Precursor"]
+      "? "]
+     [:p
+      "Tweet your design and include the hashtag #madewithprecursor. "
+      "We'll send you a one-of-a-kind shirt for free if we feature it! "]]
 
     [:figure
      [:a {:href "/blog/ideas-are-made-with-precursor"
@@ -28,8 +30,4 @@
      [:p
       [:a {:href "/blog/ideas-are-made-with-precursor"
            :title "Read full article."}
-       "Learn more."]]]
-
-    )})
-
-
+       "Learn more."]]])})
