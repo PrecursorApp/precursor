0c1ae725902bf5efe4571f9bd97b1aa49216ea21
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-31 03:44:42 -0700

    add meta image

--- src/pc/views/blog/ideas_are_made_with_precursor.clj
+++ src/pc/views/blog/ideas_are_made_with_precursor.clj
@@ -45,7 +45,7 @@
   {:title "Ideas are made with Precursor."
    :blurb "I'm exceedingly curious to see how people use Precursor. Recently we've been using an internal playback tool to learn about this..."
    :author "Danny"
-   :image ""
+   :image (external-cdn-path "/blog/ideas-are-made-with-precursor/lobanovskiy.gif")
    :body
    (list
     [:article
