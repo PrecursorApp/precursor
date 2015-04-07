99b7a1adfec4447a1d06d381de13c5a1d55f8d6e
danny <danny.newidea@gmail.com>
2015-04-06 18:17:45 -0700

    make prcrsr bot blue

--- src-cljs/frontend/state.cljs
+++ src-cljs/frontend/state.cljs
@@ -23,7 +23,7 @@
    :show-mouse? true
    :hide-in-list? true
    :cust/uuid "prcrsr-subscriber-bot"
-   :cust/color-name :color.name/green
+   :cust/color-name :color.name/blue
    :client-id "prcrsr-subscriber-bot"})
 
 (defn initial-state []
