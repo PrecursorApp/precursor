779b4dd61922539a4d91475e09e0d88b1fd309ac
Daniel Woelfel <dwwoelfel@gmail.com>
2015-04-07 23:56:07 -0700

    nicer styles

--- src-cljs/frontend/components/connection.cljs
+++ src-cljs/frontend/components/connection.cljs
@@ -39,76 +39,90 @@
         (html
          [:div.menu-view
           [:div.content
-           [:h4.make "Server Connection"]
+           [:h3.make "Server Connection"]
            [:p.make
             "Debug information about the persistent connection to Precursor's backend service."
-            [:table.shortcuts-items
+            [:table.connection-items
              [:tbody
               [:tr.make
+               [:th {:colSpan 2}
+                "Socket"]]
+              [:tr.make
                [:td [:div "type"]]
-               [:td [:div.shortcuts-result (str (:type sente-state))]]]
+               [:td [:div.connection-result (str (:type sente-state))]]]
               [:tr.make
                [:td [:div "open?"]]
-               [:td [:div.shortcuts-result (str (:open? sente-state))]]]
+               [:td [:div.connection-result (str (:open? sente-state))]]]
               [:tr.make
                [:td [:div "destroyed?"]]
-               [:td [:div.shortcuts-result (str (:destroyed? sente-state))]]]
+               [:td [:div.connection-result (str (:destroyed? sente-state))]]]
               [:tr.make
                [:td [:div "Last message"]]
-               [:td [:div.shortcuts-result (some->> sente-state
+               [:td [:div.connection-result (some->> sente-state
                                              :last-message
                                              :time
                                              (goog.date.DateTime.)
                                              (time-format/unparse (time-format/formatter "M/d/yyyy h:mm:ssa")))]]]]]]
-           [:h4.make "WebRTC Connection"]
+           [:h3.make "WebRTC Connection"]
            [:p.make
-            "Debug information about the connection between your browser and your collaborator's browsers. This connection isn't always present."
-            (for [conn-stats (:connections rtc-stats)]
-              (list
-               [:h5.make
-                "Connection from " (client-id->user app (:producer conn-stats))
-                " to " (client-id->user app (:consumer conn-stats)) "."]
-               [:p.make
-                [:table.shortcuts-items.make
-                 [:tbody
-                  [:tr.make
-                   [:td [:div "connection state"]]
-                   [:td [:div.shortcuts-result (:connection-state conn-stats)]]]
-                  [:tr.make
-                   [:td [:div "gathering state"]]
-                   [:td [:div.shortcuts-result (:gathering-state conn-stats)]]]
-                  [:tr.make
-                   [:td [:div "signaling state"]]
-                   [:td [:div.shortcuts-result (:signaling-state conn-stats)]]]]]
-                (when-let [track (some-> (or (some-> conn-stats :local-streams first)
-                                             (some-> conn-stats :remote-streams first))
-                                   :tracks
-                                   first)]
-                  (list
-                   [:p.make
-                    "Stream:"
-                    [:table.shortcuts-items.make
-                     [:tbody
-                      [:tr.make
-                       [:td [:div "enabled"]]
-                       [:td [:div.shortcuts-result (str (:enabled track))]]]
-                      [:tr.make
-                       [:td [:div "kind"]]
-                       [:td [:div.shortcuts-result (:kind track)]]]
-                      [:tr.make
-                       [:td [:div "label"]]
-                       [:td [:div.shortcuts-result {:title (:label track)} (gstring/truncate (:label track) 30)]]]]]]))
-                (for [[stat stat-map] (reverse (:stats conn-stats))]
-                  (list
-                   [:p.make
-                    [:table.shortcuts-items.make
-                     [:tbody
-                      [:tr.make
-                       [:td "Stat Name"]
-                       [:td stat]]
-                      (for [[k v] stat-map
-                            :let [v (str v)]]
-                        [:tr.make
-                         [:td [:div {:title k} (gstring/truncate k 15)]]
-                         [:td [:div.shortcuts-result {:title v}
-                               (gstring/truncate v 20)]]])]]]))]))]]])))))
+            "Debug information about the connection between your browser and your collaborator's browsers. This connection isn't always present."]
+
+           (for [conn-stats (:connections rtc-stats)
+                 :let [conn-id (str (:producer conn-stats)
+                                    (:consumer conn-stats))]]
+             [:div {:key conn-id}
+              [:h4.make {:style {:cursor "pointer"}
+                         :key "header"
+                         :on-click #(om/update-state! owner :hidden-stats
+                                                      (fn [s]
+                                                        (if (contains? s conn-id)
+                                                          (disj s conn-id)
+                                                          (conj (or s #{}) conn-id))))}
+               "Connection from " (client-id->user app (:producer conn-stats))
+               " to " (client-id->user app (:consumer conn-stats)) "."]
+              (when-not (contains? (om/get-state owner :hidden-stats) conn-id)
+                [:div
+                 [:table.connection-items.make {:key "conn"}
+                  [:tbody
+                   [:tr.make
+                    [:th {:colSpan 2}
+                     "Peer connection"]]
+                   [:tr.make
+                    [:td [:div "connection state"]]
+                    [:td [:div.connection-result (:connection-state conn-stats)]]]
+                   [:tr.make
+                    [:td [:div "gathering state"]]
+                    [:td [:div.connection-result (:gathering-state conn-stats)]]]
+                   [:tr.make
+                    [:td [:div "signaling state"]]
+                    [:td [:div.connection-result (:signaling-state conn-stats)]]]]]
+                 (when-let [track (some-> (or (some-> conn-stats :local-streams first)
+                                              (some-> conn-stats :remote-streams first))
+                                    :tracks
+                                    first)]
+                   [:table.connection-items.make {:key "stream-stats"}
+                    [:tbody
+                     [:tr.make
+                      [:th {:colSpan 2}
+                       "Stream"]]
+                     [:tr.make
+                      [:td [:div "enabled"]]
+                      [:td [:div.connection-result (str (:enabled track))]]]
+                     [:tr.make
+                      [:td [:div "kind"]]
+                      [:td [:div.connection-result (:kind track)]]]
+                     [:tr.make
+                      [:td [:div "label"]]
+                      [:td [:div.connection-result {:title (:label track)} (gstring/truncate (:label track) 30)]]]]])
+                 (for [[stat stat-map] (reverse (:stats conn-stats))]
+                   [:table.connection-items.make {:key stat}
+                    [:tbody
+                     [:tr.make
+                      [:th {:colSpan 2}
+                       [:div stat]]]
+                     (for [[k v] stat-map
+                           :let [v (str v)]]
+                       [:tr.make
+                        [:td [:div {:title k} (gstring/truncate k 15)]]
+                        [:td [:div.connection-result {:title v}
+                              (gstring/truncate v 20)]]])]])])])]])))))
