3baaf46490c3ea15984fa1d92ceb55b77e8d5d32
Daniel Woelfel <dwwoelfel@gmail.com>
2015-05-14 18:35:09 -0700

    keep track of admin urls in Mixpanel

---
+++ src/pc/http/admin/urls.clj
@@ -0,0 +1,18 @@
+(ns pc.http.admin.urls
+  (:require [cemerick.url :as url]
+            [pc.profile :as profile]))
+
+(defn make-url [path & {:keys [query]}]
+  (str (url/map->URL (merge {:host (profile/admin-hostname)
+                             :protocol (if (profile/force-ssl?)
+                                         "https"
+                                         "http")
+                             :port (if (profile/force-ssl?)
+                                     (profile/admin-https-port)
+                                     (profile/admin-http-port))
+                             :path path}
+                            (when query
+                              {:query query})))))
+
+(defn cust-info-from-cust [cust]
+  (make-url (str "/user/" (:cust/email cust))))
