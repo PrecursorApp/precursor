3a73fa0ad13696b59dceae2a4a564bc3affc2509
Daniel Woelfel <dwwoelfel@gmail.com>
2015-05-20 12:20:30 -0700

    migration to add titles to issue docs

--- src/pc/datomic/migrations.clj
+++ src/pc/datomic/migrations.clj
@@ -161,6 +161,17 @@
                       :db/unique :db.unique/value
                       :db.alter/_attribute :db.part/db}]))
 
+(defn add-titles-to-issue-docs [conn]
+  (let [db (d/db conn)
+        issues-and-docs (filter #(= "Untitled" (:document/name (:doc %)))
+                                (map (fn [d]
+                                       {:doc (d/entity db (:v d))
+                                        :issue (d/entity db (:e d))})
+                                     (d/datoms db :aevt :issue/document)))]
+    @(d/transact conn (map (fn [{:keys [doc issue]}]
+                             [:db/add (:db/id doc) :document/name (:issue/title issue)])
+                           issues-and-docs))))
+
 (def migrations
   "Array-map of migrations, the migration version is the key in the map.
    Use an array-map to make it easier to resolve merge conflicts."
@@ -177,7 +188,8 @@
    9 #'add-prcrsr-bot-color
    10 #'add-team-plans
    11 #'ensure-team-creators
-   12 #'add-access-grant-indexes))
+   12 #'add-access-grant-indexes
+   13 #'add-titles-to-issue-docs))
 
 (defn necessary-migrations
   "Returns tuples of migrations that need to be run, e.g. [[0 #'migration-one]]"
