56c23f8373028767155c84098d70d9ab42ff07bf
Daniel Woelfel <dwwoelfel@gmail.com>
2015-06-11 23:50:31 -0700

    require csrf tokens for ws

--- src/pc/http/routes/talaria.clj
+++ src/pc/http/routes/talaria.clj
@@ -3,15 +3,21 @@
             [defpage.core :as defpage :refer (defpage)]
             [immutant.web.async :as immutant]
             [pc.http.talaria :as tal]
-            [pc.rollbar :as rollbar])
+            [pc.rollbar :as rollbar]
+            [ring.middleware.anti-forgery :as csrf]
+            [crypto.equality :as crypto])
   (:import [java.util UUID]))
 
 (defpage "/talaria" [req]
-  (let [channel-id (UUID/randomUUID)]
-    (immutant/as-channel (assoc req :tal/ch-id channel-id)
-                         {:on-open (tal/handle-ws-open tal/talaria-state)
-                          :on-error (tal/handle-ws-error tal/talaria-state)
-                          :on-close (tal/handle-ws-close tal/talaria-state)
-                          :on-message (tal/handle-ws-msg tal/talaria-state)})))
+  (if (crypto/eq? (pc.utils/inspect (get-in req [:params :csrf-token]))
+                  csrf/*anti-forgery-token*)
+    (let [channel-id (UUID/randomUUID)]
+      (immutant/as-channel (assoc req :tal/ch-id channel-id)
+                           {:on-open (tal/handle-ws-open tal/talaria-state)
+                            :on-error (tal/handle-ws-error tal/talaria-state)
+                            :on-close (tal/handle-ws-close tal/talaria-state)
+                            :on-message (tal/handle-ws-msg tal/talaria-state)}))
+    {:status 400
+     :body "Invalid CSRF token"}))
 
 (def app (defpage/collect-routes))
