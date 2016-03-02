53f10b31a12a69a5227bbefdbbf0e5ec756c6f81
Daniel Woelfel <dwwoelfel@gmail.com>
2016-03-01 22:18:57 -0800

    be less lazy

--- src/pc/mailgun.clj
+++ src/pc/mailgun.clj
@@ -1,12 +1,9 @@
 (ns pc.mailgun
   (:require [cheshire.core :as json]
             [clj-http.client :as http]
-            [pc.profile]))
-
-(def base-url pc.profile/mailgun-base-url)
-
-(def api-key pc.profile/mailgun-api-key)
+            [pc.profile :as profile]))
 
 (defn send-message [{:keys [from to cc bcc subject text html] :as props}]
-  (http/post (str (base-url) "messages") {:basic-auth ["api" (api-key)]
-                                          :form-params props}))
+  (http/post (str (profile/mailgun-base-url) "messages")
+             {:basic-auth ["api" (profile/mailgun-api-key)]
+              :form-params props}))
