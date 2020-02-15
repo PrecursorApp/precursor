91bf2bc49504b7d24afd823b5196223f123a6b57
Daniel Woelfel <dwwoelfel@gmail.com>
2020-02-15 15:48:05 -0800

    another try at setting srcObject on audio

--- src-cljs/frontend/components/rtc.cljs
+++ src-cljs/frontend/components/rtc.cljs
@@ -3,6 +3,21 @@
             [om.core :as om]
             [om.dom :as dom]))
 
+(defn audio [stream owner]
+  (reify
+    om/IDidMount
+    (did-mount [_]
+      (when-let [audio (om/get-node owner "audio")]
+        (aset audio "srcObject" stream)))
+    om/IDidUpdate
+    (did-update [_ _ _]
+      (when-let [audio (om/get-node owner "audio")]
+        (aset audio "srcObject" stream)))
+    om/IRender
+    (render [_]
+      (dom/audio #js {:autoPlay true
+                      :ref "audio"}))))
+
 (defn rtc [app owner]
   (reify
     om/IDisplayName (display-name [_] "RTC")
@@ -11,6 +26,6 @@
       (apply dom/div nil
              (for [{:keys [stream]} (vals (get-in app [:subscribers :info]))
                    :when stream]
-               (dom/audio #js {:autoPlay true
-                               :key (aget stream "id")
-                               :ref (fn [r] (when r (aset r "srcObject" stream)))}))))))
+               (om/build audio
+                         stream
+                         {:react-key (aget stream "id")}))))))
